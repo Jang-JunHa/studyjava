0be4f474349ffb9c765e5272e9c75ebfe71f21b6
studyjava <wkdwnsgksla7@gmail.com>
2023-07-28 15:26:44 +0900

    변수형

---
+++ src/main/java/ch2/VarEx1.java
@@ -0,0 +1,21 @@
+package ch2;
+
+public class VarEx1 {
+public static void main(String[] args){
+  /*2023-07-28 이클립스에서 사용하는 syso 대신 sout 라 치고 컨트롤+스페이스 를 누르면 자동완성이 된다.*/
+
+    int year = 0;
+    int age = 14;
+
+    System.out.println(year);
+    System.out.println(age);
+
+    year = age +2000;
+    age = age +1;
+
+    System.out.println(year);
+    System.out.println(age);
+
+
+}
+}
--- src/main/java/org/studyjava/Main.java
+++ src/main/java/org/studyjava/Main.java
@@ -1,10 +1,11 @@
 package org.studyjava;
 
+import ch2.VarEx1;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
+//        System.out.println("Hello world!");
     }
 
 }
